355c170988b0585845535bae359bee08ed55a6ae
nboutin <boutwork@gmail.com>
2015-07-10 10:34:02 +0200

    Added horizontal victory detection

--- tic_tac_toe/src/main.cpp
+++ tic_tac_toe/src/main.cpp
@@ -11,12 +11,15 @@
 #include <iostream>
 #include <string>
 #include <sstream>
-#include "engine/engine.h"
+
+#include "engine/engine.hpp"
+#include "print.hpp"
 
 ///	=== Namespaces	================================================================================
 
 using namespace std;
 using namespace engine;
+using namespace print;
 
 ///	===	Public Definitions	========================================================================
 
@@ -24,26 +27,33 @@ int main()
 {
 	cout << "TIC TAC TOE" << endl;
 
-	auto engine = Engine();
+	auto engine = Engine("P1", "P2");
 	auto is_quit = false;
-	auto str_input = string {};
+	auto str_input = string { };
 	auto x = 0U, y = 0U;
 	auto engine_result = false;
 
 	while (engine.is_game_finished() == false && is_quit == false)
 	{
+		cout << "=====\t" << engine.get_current_player() << "\t=====" << endl;
 
 		do
 		{
-			/// Choose action
-			cout << "Select cell(x y):\t   ";
+			cout << "Select cell(x y): ";
 			getline(cin, str_input);
 			stringstream(str_input) >> x >> y;
 
 			engine_result = engine.add_pawn(x, y);
 		}
 		while (engine_result == false);
+
+		print_grid(engine);
 	}
+
+	/// Display end of game
+	cout << "Winner is " << engine.get_winner() << endl;
+
+	cout << "===== End of Game =====" << endl;
 
 	return 0;
 }
