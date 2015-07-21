755a7d765f758a036e94ca3931b2f17b20c7603e
nboutin <boutwork@gmail.com>
2015-07-21 10:29:24 +0200

    Added minmax
    Basic eval function

---
+++ connect_4/src/min_max_impl/engine_mmg.cpp
@@ -0,0 +1,81 @@
+///
+/// \file	ai_engine_wrapper.cpp
+///	\brief	
+///	\date	16 juil. 2015
+/// \author	nboutin
+///
+
+#include <min_max_impl/engine_mmg.hpp>
+using namespace connect_4;
+
+/// === Includes	================================================================================
+
+#include "engine/engine.hpp"
+
+/// === Namespaces	================================================================================
+
+using namespace std;
+using namespace ai;
+
+/// === Public Definitions	========================================================================
+
+Engine_MMG::Engine_MMG(Engine& _engine, const string& _ai_player)
+		: engine_(_engine), ai_player_(_ai_player)
+{
+}
+
+///	------------------------------------------------------------------------------------------------
+
+vector<uint8_t> Engine_MMG::get_moves() const
+{
+	auto moves = vector<uint8_t> { };
+
+	for (auto x = uint8_t { }; x < Engine::width; ++x)
+	{
+		if (engine_.is_valid_move(x) == true)
+		{
+			moves.push_back(x);
+		}
+	}
+
+	return moves;
+}
+
+///	------------------------------------------------------------------------------------------------
+
+void Engine_MMG::execute_move(const uint8_t& _move) const
+{
+	engine_.add_pawn(_move);
+}
+
+///	------------------------------------------------------------------------------------------------
+
+void Engine_MMG::undo_move(const uint8_t& _move) const
+{
+	engine_.remove_pawn(_move);
+}
+
+///	------------------------------------------------------------------------------------------------
+
+int16_t Engine_MMG::eval() const
+{
+	auto n_cell = uint16_t { };
+
+	/// Count active pawn
+	for (auto& line : engine_.get_grid())
+		for (auto cell : line)
+			if (cell != e_pawn::none) n_cell++;
+
+	auto winner = engine_.get_winner();
+	auto result = int16_t { };
+
+	if (winner == ai_player_) result = 1000 - n_cell;
+	else if (winner == "draw") result = 0;
+	else result = -1000 + n_cell;
+
+	return result;
+}
+
+/// === Private Definitions	========================================================================
+
+/// === END OF FILES	============================================================================
---
+++ connect_4/src/min_max_impl/engine_mmg.hpp
@@ -0,0 +1,50 @@
+///
+/// \file	ai_engine_wrapper.h
+///	\brief	
+///	\date	16 juil. 2015
+/// \author	nboutin
+///
+#ifndef ENGINE_GMM_HPP_
+#define ENGINE_GMM_HPP_
+
+/// === Includes	================================================================================
+
+#include "min_max_generic/min_max_engine.hpp"
+#include "engine/engine.hpp"
+
+/// === Namespaces	================================================================================
+namespace connect_4
+{
+
+/// === Class Declarations	========================================================================
+class Engine_MMG : public ai::min_max::Min_Max_Engine<uint8_t>
+{
+public:
+	/// === Public Declarations	====================================================================
+
+	Engine_MMG(Engine& _engine, const std::string& _ai_player);
+
+	virtual std::vector<uint8_t> get_moves() const;
+	virtual void execute_move(const uint8_t& _move) const;
+	virtual void undo_move(const uint8_t& _move) const;
+	virtual int16_t eval() const;
+
+	virtual bool is_game_finished() const
+	{
+		return engine_.is_game_finished();
+	}
+
+private:
+	/// === Private Declarations	================================================================
+	/// === Private Attributs	====================================================================
+
+	Engine& engine_;
+	std::string ai_player_;
+};
+
+///	------------------------------------------------------------------------------------------------
+
+}
+
+#endif
+/// === END OF FILE	================================================================================
